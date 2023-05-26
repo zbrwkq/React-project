c148a7cdb4cd45d81f01e40b031a9945cef2a35a
r-tisba <81628282+r-tisba@users.noreply.github.com>
2023-05-26 17:01:02 +0200

    fix entreprise.js

--- src/components/pages/entreprise.js
+++ src/components/pages/entreprise.js
@@ -26,7 +26,7 @@ const Entreprise = () => {
             {data.headquarters && (
                 <SectionImage image={header}>
                     <div className='divEntreprise'>
-                        <div className="company-info">
+                        <div className="company-info blur">
                             <div className="info">
                                 <div className="company-name">
                                     <span className='spaceX'>{data.name}</span>, fondé en {data.founded} par {data.founder}
@@ -46,14 +46,19 @@ const Entreprise = () => {
                             <div className="summary">{data.summary}</div>
                             <div className="valuation">Valorisation : {bigNumber(data.valuation)}$</div>
                         </div>
-                        <div className='card'>
+                        <div className='card company-info2 blur'>
                             <div className="executives">
-                                <div>CEO : {data.ceo}</div>
-                                <div>CTO : {data.cto}</div>
-                                <div>COO : {data.coo}</div>
-                                <div>CTO Propulsion : {data.cto_propulsion}</div>
+                                <div className="">
+                                    <img className='portraitExecutives' src='https://upload.wikimedia.org/wikipedia/commons/e/ed/Elon_Musk_Royal_Society.jpg'></img><div className='m-3'>CEO & CTO : {data.ceo}</div>
+                                </div>
+                                <div className="">
+                                    <img className='portraitExecutives' src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Gwynne_Shotwell_at_pre-launch_briefing_for_CRS-2_mission_%28KSC-2013-1704%29.jpg/800px-Gwynne_Shotwell_at_pre-launch_briefing_for_CRS-2_mission_%28KSC-2013-1704%29.jpg"></img><div className='m-3'>COO : {data.coo}</div>
+                                </div>
+                                <div className="">
+                                    <img className='portraitExecutives' src='https://www.uidaho.edu/-/media/UIdaho-Responsive/Images/engr/news/features/tom-mueller/tom-mueller-profile.jpg'></img><div className='m-3'>CTO Propulsion : {data.cto_propulsion}</div>
+                                </div>
                             </div>
-                            <div className="useful-links">
+                            <div className="useful-links text-center mt-2">
                                 Liens utiles :
                                 <a href={data.links.website}>Site</a>
                                 <a href={data.links.flickr}>Flickr</a>
